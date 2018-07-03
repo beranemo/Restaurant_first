c16d4f1c938d079eafb1d0a6b96e5eed71bb1336
julia <skylife21666@gmail.com>
2018-07-03 18:55:29 +0800

    add new and create actions and views for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root "restaurants#index"
   resources :restaurants, only: [:index, :show]
 
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
     resources :restaurants
     root "restaurants#index"
   end
-
 end
