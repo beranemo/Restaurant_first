7d92a7f3b88ede55b414578c22ec0e6305f30d07
julia <skylife21666@gmail.com>
2018-07-10 17:02:21 +0800

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704160210) do
+ActiveRecord::Schema.define(version: 20180710085149) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
